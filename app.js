b971371a37ab300f7be82ceb7c7b21a61a3b829e
Brian Smith <briaguya@gmail.com>
2018-06-02 16:55:03 -0500

    come back to me cors!

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
 const index = require("./routes/index");
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
