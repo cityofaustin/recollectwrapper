ef268b65b1689b5799929dc294fd1db685ef71c9
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2018-06-02 15:47:29 -0500

    adding map and filter to remove null return from calendar endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,54 @@
-const express = require('express');
-const querystring = require('querystring');
-const request = require('../lib/request');
+const express = require("express");
+const querystring = require("querystring");
+const request = require("../lib/request");
 const router = express.Router();
 
-let config = require('../lib/config');
+let config = require("../lib/config");
 
-router.post('/suggest', (req, res) => {
-
+router.post("/suggest", (req, res) => {
   let addressQuery = querystring.escape(req.body.address);
   config.path = `/api/areas/Austin/services/323/address-suggest?q=${addressQuery}`;
 
-  request(config)
-  .then(results => {
-    let data = results.map(element => ({
-      parcel_id: element.parcel_id,
-      name: element.name,
-      area_id: element.area_id,
-      service_id: element.service_id
-    }));
-    res.json(data);
-  })
-  .catch(err => {
-    res.json(err)
-  });
+  request(config).then(results => {
+      let data = results.map(element => ({
+        parcel_id: element.parcel_id,
+        name: element.name,
+        area_id: element.area_id,
+        service_id: element.service_id
+      }));
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
-router.post('/address', (req, res) => {
+router.post("/address", (req, res) => {
   let serviceId = req.body.service_id;
   let parcelId = req.body.parcel_id;
   let areaId = req.body.area_id;
 
   config.path = `/api/places?parcel_id=${parcelId}&area_id=${areaId}&svc_id=${serviceId}`;
 
-  request(config)
-  .then(result => {
-    res.json(result.place.id);
-  })
-  .catch(err => {
-    res.json(err)
-  });
+  request(config).then(result => {
+      res.json(result.place.id);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
-router.post('/calendar', (req, res) => {
-  config.path = `/api/places/${req.body.id}/services/323/events?hide=reminder_only`;
+router.post("/calendar", (req, res) => {
+    config.path = `/api/places/${req.body.id}/services/323/events?hide=reminder_only&after=2018-01-01&before=2019-12-31`;
 
-  request(config)
-  .then(results => {
-    let data = results.events.map(element => ({
-      day: element.day,
-      pickup_types: element.flags.map(el => el.name)
-    }));
-    res.json(data);
-  })
-  .catch(err => {
-    res.json(err)
+    request(config).then(results => {
+      let data = results.events.map(element => element.flags && {
+              day: element.day,
+              pickup_types: element.flags.map(el => el.name)
+            }).filter(element => element);
+            console.log(data);
+            res.json(data);
+    });
   });
-});
 
 module.exports = router;
