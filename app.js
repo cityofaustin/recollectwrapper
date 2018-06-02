6dcb880bb7225393c748cd5239671591d9eb3261
Brian Smith <briaguya@gmail.com>
2018-06-02 16:08:07 -0500

    cors

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
-const index = require('./routes/index');
+const index = require("./routes/index");
 
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', index);
+app.use("/api", index);
 
 module.exports = app;
