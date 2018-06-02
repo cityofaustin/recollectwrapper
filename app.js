4120d66a009599ce0df01da0c53b8133abcf41e6
Brian Smith <briaguya@gmail.com>
2018-06-02 16:10:26 -0500

    cors?

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const cors = require("cors");
 
 const app = express();
 
 const index = require("./routes/index");
 
-app.use(cors());
+app.use(allowCrossDomain);
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
